0ffd1a32ccdfff4571b4d1e3055167917eae82e0
hboppe <hanna.nascimento31@gmail.com>
2022-10-11 22:40:59 -0400

    Finalizei exercicio s3-23

---
+++ s3-23/script.js
@@ -0,0 +1,97 @@
+// EXERCICIO 1
+
+// let numero = parseInt(prompt("Digite um numero inteiro"));
+
+// alert(numero);
+
+// EXERCICIO 2
+
+// let numero1 = parseInt(prompt("Digite um numero inteiro")) 
+
+// let numero2 = parseInt(prompt("Digite o segundo numero inteiro"))
+
+// let numero3 = parseInt(prompt("Digite o terceiro numero inteiro"))
+
+// let soma = numero1 + numero2 + numero3;
+
+// alert(`A soma dos números é ${soma}`)
+
+// EXERCICIO 3
+
+// let nota1 = parseFloat(prompt(`Digite uma nota`));
+
+// let nota2 = parseFloat(prompt(`Digite a segunda nota`));
+
+// let media = (nota1 + nota2)/2;
+
+// alert(`A media das notas ${nota1} e ${nota2} é ${media}`)
+
+// EXERCICIO 4
+
+// let num = parseInt(prompt(`Digite um numero inteiro`));
+
+// let resto = num % 2;
+
+// alert(`O resto da divisao de ${num} por 2 é ${resto}`)
+
+// EXERCICIO 5
+
+// let num = parseInt(prompt(`Digite um numero inteiro`))
+
+// let quad = num ** 2;
+
+// alert(`${num} elevado ao quadrado é ${quad}`);
+
+// EXERCICIO 6
+
+// let num1 = Number(prompt(`Digite o primeiro numero`));
+// let num2 = Number(prompt(`Digite o segundo numero`));
+// let num3 = Number(prompt(`Digite o terceiro numero`));
+// let num4 = Number(prompt(`Digite o quarto numero`));
+
+// let mediaPond = (num1 + num2 * 2 + num3 * 3 + num4 * 4)/10
+
+// alert(`A media ponderada das notas é ${mediaPond}`)
+
+// EXERCICIO 7
+
+// let math = parseFloat(prompt(`Digite sua nota de Matematica`));
+
+// let port = parseFloat(prompt(`Digite sua nota de Portugues`));
+// let gerais = parseFloat(prompt(`Digite sua nota de Conhecimentos gerais`));
+// let inf = parseFloat(prompt(`Digite sua nota de Informatica`));
+
+// let mediaPond = ((math * 2) + port + gerais + inf)/5;
+
+// alert(`Sua media ponderada é ${mediaPond}`)
+
+// EXERCICIO 8
+
+// let dividendo = parseInt(prompt(`Digite um numero inteiro para ser o dividendo`));
+
+// let divisor = parseInt(prompt(`Digite um numero inteiro para ser o divisor`));
+
+// let divisao = dividendo/divisor;
+
+// let divisaoInt = Math.floor(dividendo/divisor);
+
+// let resto = dividendo % divisor;
+
+// alert(`Divisao: ${divisao}; Divisao inteira: ${divisaoInt}; Resto da divisao: ${resto}`)
+
+// EXERCICIO 9
+
+// let aplic = Number(prompt(`Digite o saldo da sua aplicacao`))
+
+// let saldo = aplic * 1.01;
+
+// alert(`O novo saldo da aplicacao é de ${saldo}`)
+
+// EXERCICIO 10
+
+// let d1 = Number(promp(`Digite a medida da primeira diagnonal do losango`))
+// let d2 = Number(promp(`Digite a medida da segunda diagnonal do losango`))
+
+// let area = (d1 * d2)/ 2
+
+// alert(`A área total do losando é de ${area}`)
