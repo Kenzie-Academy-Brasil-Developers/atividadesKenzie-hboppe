2149d9d2dfa3d1eba56f9fba73ce88e1d855a0b8
hboppe <hanna.nascimento31@gmail.com>
2022-10-13 20:20:29 -0400

    Entrega da atividade s3-31

---
+++ s3-31/script.js
@@ -0,0 +1,119 @@
+// EXERCICIO 1
+
+// function imprimNum() {
+//     let numero = parseInt(prompt(`Digite um número`));
+
+//     alert(numero);
+// }
+
+// imprimNum()
+
+// EXERCICIO 2
+
+// function somaDeNum(num1, num2, num3){
+//     alert(num1 + num2 + num3)
+// }
+
+// let numero1 = parseInt(prompt(`Digite o primeiro numero`))
+// let numero2 = parseInt(prompt(`Digite o segundo numero`))
+// let numero3 = parseInt(prompt(`Digite o terceiro numero`))
+
+// somaDeNum(numero1, numero2, numero3);
+
+// EXERCICIO 3
+
+// function mediaAnual(nota1, nota2){
+//     let media = (nota1+nota2)/ 12;
+//     alert(media.toFixed(2))
+// }
+
+// let notaSem1 = parseInt(prompt(`Digite a primeira nota semestral`))
+
+// let notaSem2 = parseInt(prompt(`Digite a segunda nota semestral`))
+
+// mediaAnual(notaSem1, notaSem2);
+
+// EXERCICIO 4
+
+// function restoDiv(num1, num2){
+//     let resultado = num1 % num2;
+//     if (resultado == 0){
+//         checarResto();
+//     } else {
+//         divInex(resultado)
+//     }
+// }
+
+// function divExata(){
+//     alert(`Esta é uma divisão exata`)
+// }
+
+// function divInex(resultado){
+//     alert(`Esta é uma divisão inexata e o resto é ${resultado}`)
+// }
+
+// restoDiv(11, 2)
+
+// EXERCICIO 5
+
+// function quad(numero){
+//     let numAoQuad = Math.pow(numero, 2)
+//     alert(`O quadrado de ${numero} é ${numAoQuad}`)
+// }
+
+// quad(2);
+
+// EXERCICIO 6
+
+// function mediaPonderada (num1, num2, num3, num4){
+//     let media = ((num1) + (num2 * 2) + (num3 * 3) + (num4 * 4))/10;
+//     alert(`A media ponderada é: ${media}`)
+// }
+
+// let numero1 = Number(prompt(`Digite a primeira nota`))
+// let numero2 = Number(prompt(`Digite a segunda nota`))
+// let numero3 = Number(prompt(`Digite a terceira nota`))
+// let numero4 = Number(prompt(`Digite a quarta nota`))
+
+// mediaPonderada(numero1, numero2, numero3, numero4);
+
+// EXERCICIO 7
+
+// function mediaPonderada(notaMath, notaPort, notaCG, notaInf){
+//     let media = ((notaMath * 2) + notaPort + notaCG + notaInf)/ 5;
+//     alert(`A media ponderada é de ${media.toFixed(2)}`)
+// }
+
+// let math = Number(prompt('Digite a nota de matematica'))
+// let port = Number(prompt('Digite a nota de Portugues'))
+// let cg = Number(prompt('Digite a nota de conhecimentos gerais'))
+// let inf = Number(prompt('Digite a nota de informatica'))
+
+// mediaPonderada(math, port, cg, inf);
+
+// EXERCICIO 8
+
+// function novoSaldo(){
+//     let saldo = Number(prompt(`Digite o saldo da aplicaçao`))
+
+//     let saldoAtualizado = saldo * 1.03;
+//     alert(`O novo salário é de: R$ ${saldoAtualizado.toFixed(2)}`)
+// }
+
+// novoSaldo()
+
+// EXERCICIO 9
+
+
+// function areaTriang(){
+//     let base = Number(prompt(`Digite o valor da base`))
+//     let altura = Number(prompt(`Digite o valor da altura`))
+//     let area = (base * altura)/2
+
+//     alert(`A area do triangulo é: ${area}`)
+// }
+
+// let areaTotal = areaTriang();
+
+// EXERCICIO 10
+
