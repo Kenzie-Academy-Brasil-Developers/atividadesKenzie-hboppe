7297aa5f99db83652e55d46b1f341518198850a8
hboppe <hanna.nascimento31@gmail.com>
2022-10-17 19:48:10 -0400

    Fiz ate exercicio 6.

--- s4-08/script.js
+++ s4-08/script.js
@@ -25,16 +25,58 @@
 
 ///////////////////////EXERCICIO 3/////////////////
 
-function incrementTextBackwards(string){
-    let newString = string[string.length -1]; // k
-    console.log()
-    for (let i = string.length - 2; i >= -1; i--){
+// function incrementTextBackwards(string){
+//     let newString = string[string.length -1]; // k
+//     console.log()
+//     for (let i = string.length - 2; i >= -1; i--){
         
-        console.log(newString)
-        newString = string[i] + newString
+//         console.log(newString)
+//         newString = string[i] + newString
 
     
-    }
-}
+//     }
+// }
 
-incrementTextBackwards("Fullstack");
+// incrementTextBackwards("Fullstack");
+
+///////////////////////EXERCICIO 4/////////////////
+
+// function decrementText(string){
+
+//     for (let i = string.length; i >= 0; i--){
+//         let newString = string.substring(0,i)
+//         console.log(newString)
+
+//     }
+
+// }
+
+// decrementText("Fullstack")
+
+///////////////////////EXERCICIO 5/////////////////
+
+// function incrementTextMiddle(string){
+
+//     if (string.length % 2 == 0){
+//         return "Palavra Inválida";
+//     } else {
+//         let index = Math.floor(string.length/2);
+//         let left = index
+//         let termo1 = ''
+//         let indexDoMeio = string[index]
+//         for (let i = index; i < string.length; i++){
+//             left--
+//             console.log(indexDoMeio)
+//             termo1 = string[left] + indexDoMeio + string[i+1]
+//             indexDoMeio = termo1;
+            
+            
+//         }
+//     }
+// }
+
+// incrementTextMiddle("Fullstack")
+
+function padEnd(){
+    
+}
