d26a309fc25e6ecde5f6157110f8c94c51684930
hboppe <hanna.nascimento31@gmail.com>
2022-10-17 14:57:08 -0400

    finalizei a atividade s4-07

--- s4-07/script.js
+++ s4-07/script.js
@@ -22,17 +22,58 @@
 
 /////////////////////EXERCICIO 3////////////////////
 
-let numero = Number(prompt(`Digite um numero aqui`))
-let soma = 0;
-let cont = 0;
+// let numero = Number(prompt(`Digite um numero aqui`))
+// let soma = 0;
+// let cont = 0;
 
-while(numero == parseInt(numero) && numero > 0 && numero < 10){
-    soma += numero;
-    cont++
-}
+// while(numero == parseInt(numero) && numero > 0 && numero < 10){
+//     soma += numero;
+//     cont++
+//     numero = Number(prompt(`Digite um numero aqui`))
+// }
 
-let media = soma/ cont;
+// let media = soma/ cont;
 
-alert(`A soma dos numeros é ${soma} e a media deles é de ${media}`)
+// alert(`A soma dos numeros é ${soma} e a media deles é de ${media}`)
+
+/////////////////////EXERCICIO 4////////////////////
+
+// let a = 0;
+// let b = 0;
+// let c = 0;
+// let voto = prompt(`Digite a letra (a, b ou c) referente ao seu candidato`)
+
+// while (voto != 'x'){
+//     voto = prompt(`Digite a letra (a, b ou c) referente ao seu candidato`)
+//     if (voto == 'a'){
+//         a++
+//         alert(`Voto registrado com sucesso`)
+//     } else if (voto == 'b'){
+//         b++
+//         alert(`Voto registrado com sucesso`)
+//     } else if (voto == 'c'){
+//         c++
+//         alert(`Voto registrado com sucesso`)
+
+//     } else if (voto == 'x'){
+//         alert(`O candidato A teve ${a} votos. O candidato B teve ${b} votos. O candidato C teve ${c} votos`)
+//     } else {
+//         alert(`Candidato nao encontrado`)
+//     }
+// }
+
+/////////////////////EXERCICIO 5////////////////////
+
+// let temp = Number(prompt(`Digite a temperatura`))
+// let soma = 0;
+
+// while (temp != 00){
+//     soma += temp
+//     temp = Number(prompt(`Digite a temperatura`))
+    
+// }
+
+// alert(`A soma de todas as temperaturas é de ${soma}`)
+
 
 
