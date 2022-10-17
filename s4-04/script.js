4afb1e95a50f6771d8950dd3bcdbe8d8e9374a9f
hboppe <hanna.nascimento31@gmail.com>
2022-10-17 11:23:43 -0400

    Finalizei o exercicio s4-04

---
+++ s4-04/script.js
@@ -0,0 +1,98 @@
+/////////////////////EXERCICIO 1////////////////////
+
+// function numAoQuad(){
+//     for (let i = 15; i <= 200; i++){
+//         console.log(Math.pow(i,2))
+//     }
+// }
+
+// numAoQuad()
+
+/////////////////////EXERCICIO 2////////////////////
+
+// function somaDos100() {
+//     let cont = 0;
+//     for (let i = 1; i <=100; i++){
+//         cont += i
+//     }
+
+//     return cont;
+// }
+
+// console.log(somaDos100())
+
+/////////////////////EXERCICIO 3////////////////////
+
+// function divPor4 (){
+//     for (let i = 0; i < 200; i += 4){
+//         console.log(i)
+//     }
+// }
+
+// divPor4 ()
+
+/////////////////////EXERCICIO 4////////////////////
+
+// function somarDezNum(){
+
+//     let soma = 0;
+
+//     for (let i = 0; i < 10; i++){
+//         let num = Number(prompt('Digite um numero real'))
+//         soma += num;
+//     }
+
+//     let media = soma / 10
+
+//     alert(`A soma dos numero digitados é ${soma} e a media dos numeros é ${media}`)
+// }
+
+// somarDezNum()
+
+/////////////////////EXERCICIO 5////////////////////
+
+// function somaEMedia(){
+//     let soma = 0;
+//     let cont = 0;
+//     for (let i = 50; i <= 70; i +=2){
+//         soma += i;
+//         cont ++
+//     }
+//     let mediaArt = soma/cont;
+//     console.log(`A media aritmetica é ${mediaArt}`)
+// }
+
+// somaEMedia()
+
+/////////////////////EXERCICIO 6////////////////////
+
+// function somaEMedia(){
+//     let soma = 0;
+//     let cont = 0;
+//     for (let i = 51; i <= 70; i += 2){
+//         soma += i;
+//         cont ++
+//     }
+//     let mediaArt = soma/cont;
+//     console.log(`A media aritmetica é ${mediaArt}`)
+// }
+
+// somaEMedia()
+
+/////////////////////EXERCICIO 7////////////////////
+
+// function conversorDeTemp(){
+//     let fahr = 50
+//     for (let i = 10; i <= 100; i += 10){
+//         console.log(`${i}c = ${fahr}f`)
+//         fahr += 18
+//     }
+// }
+
+// conversorDeTemp()
+
+
+
+
+
+
