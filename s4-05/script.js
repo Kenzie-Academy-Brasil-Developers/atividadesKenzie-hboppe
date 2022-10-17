cc1caad1c7dc425b310c50418b407b48bedd64a6
hboppe <hanna.nascimento31@gmail.com>
2022-10-17 12:53:29 -0400

    Finalizei exercicio

---
+++ s4-05/script.js
@@ -0,0 +1,42 @@
+/////////////////EXERCICIO 1////////////////
+
+// function fibonacciNumbers(){
+//     let arr = []
+
+//     for (let i = 0; i < 15; i++){
+
+//         if (i == 0){
+//             arr.push(0)
+//         } else if (i == 1){
+//             arr.push(1)
+//         }
+//         else {
+//             let novoElem = arr[i - 1] + arr[i - 2];
+//             arr.push(novoElem)
+//         }
+        
+//     }
+
+//     console.log(arr)
+// }
+
+// fibonacciNumbers()
+
+/////////////////EXERCICIO 2////////////////
+
+// function somaGraos(){
+
+//     let termo1 = 1
+//     let termo2 = 0;
+//     let soma = termo1 + termo2
+//     let numQuadro = 64;
+//     for (let i = 0; i < numQuadro-1; i++){
+//         termo2 = termo1 * 2
+//         soma += termo2
+//         termo1 = termo2
+        
+//     }
+
+//     console.log(soma)
+// }
+// somaGraos()
