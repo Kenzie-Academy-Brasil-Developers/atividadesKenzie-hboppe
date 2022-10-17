37cfdfc1eda055418ad6766778b2682aceac4677
hboppe <hanna.nascimento31@gmail.com>
2022-10-17 10:30:58 -0400

    terminei exercicio s4-03

--- s4-03/script.js
+++ s4-03/script.js
@@ -21,6 +21,86 @@
 
 /////////EXERCICIO 3////////
 
-function oddUpToAHundred(){
-    for 
-}
+// function oddUpToAHundred(){
+//     for(let i = 0; i <= 100; i ++){
+//         if(i % 2 != 0){
+//             console.log(i)
+//         }
+//     }
+// }
+
+// oddUpToAHundred()
+
+/////////EXERCICIO 4////////
+
+// function evenUpToAHundred(num1, num2){
+//     for (let i = num1; i <= num2; i++){
+//         if (i % 2 == 0){
+//             console.log(i)
+//         }
+//     }
+
+// }
+
+// evenUpToAHundred(0, 100);
+
+/////////EXERCICIO 5////////
+
+// function fromNegativeToPositive(numNeg, numPos){
+//     for (let i = numNeg; i <= numPos; i++){
+//         console.log(i)
+        
+//     }
+// }
+
+// fromNegativeToPositive(-10, 0)
+
+/////////EXERCICIO 6////////
+
+// function inDescendingDirection(num1, num2){
+//     for (let i = -num1; i <= num2; i++){
+//         console.log(i)
+//     }
+// }
+
+// inDescendingDirection(10, 0)
+
+/////////EXERCICIO 7////////
+
+// function inDescendingDirectionTenOutOfTen(num1, num2){
+//     for (let i = num1; i >= num2; i -= 10){
+//         console.log(i)
+//     }
+// }
+
+// inDescendingDirectionTenOutOfTen(100, 10)
+
+/////////EXERCICIO 8////////
+
+// function divisibleByTwoUpToTen(){
+//     for (let i = 0; i <= 10; i += 2){
+//         console.log(i)
+//     }
+// }
+
+// divisibleByTwoUpToTen()
+
+/////////EXERCICIO 9////////
+
+// function toSquare(num1, num2){
+//     for (let i = num1; i <= num2; i++){
+//         console.log(Math.pow(i, 2))
+//     }
+// }
+
+// toSquare(0, 10)
+
+/////////EXERCICIO 10////////
+
+// function toExponent(){
+//     for (let i = 0; i <= 10; i++){
+//         console.log(Math.pow(i,i))
+//     }
+// }
+
+// toExponent()
