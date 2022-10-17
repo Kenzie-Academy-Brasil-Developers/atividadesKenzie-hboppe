0d4bbb9a639aa7c06c29a8ff7d1148d6fce0a964
hboppe <hanna.nascimento31@gmail.com>
2022-10-16 21:06:45 -0400

    Fiz ate o exercicio 2

---
+++ s4-03/script.js
@@ -0,0 +1,26 @@
+/////////////////////// ATIVIDADE S4-03/////////////////////////
+//////////EXERCICIO 1//////////
+
+// function renderUpToHundred(){
+//     for(let i=0; i<=100; i++){
+//         console.log(i)
+//     }
+// }
+
+// renderUpToHundred()
+
+//////////EXERCICIO 2///////////
+
+// function tenInTenToAHundred(){
+//     for(let i=10; i<=100; i+=10){
+//         console.log(i)
+//     }
+// }
+
+// tenInTenToAHundred()
+
+/////////EXERCICIO 3////////
+
+function oddUpToAHundred(){
+    for 
+}
