9feb37e489c4d8d1279ea3983af1afaf5ff43f23
hboppe <hanna.nascimento31@gmail.com>
2022-10-19 15:33:38 -0400

    Fiz ate exercicio 3

---
+++ s4-14/script.js
@@ -0,0 +1,42 @@
+const listaDeElementos = [1,5,7,9,3, "10", "11", "15"]
+
+///////////////EXERCICIO 1////////////
+
+// function inserirNovoElemento(num){
+//     const autput=[]
+//     autput.push(num)
+//     if(num < 1 || num > 10){
+//         alert(`Numero invalido`)
+//     } else {
+//         alert(`A lista atualizada é: ${autput}`)
+//     }
+
+//     alert(`A lista atualizada é: ${autput}`)
+// }
+
+// let numero = parseInt(prompt(`Digite um número de 1 a 10`))
+
+///////////////EXERCICIO 2////////////
+
+// function retornaTodosElementos(){
+//     alert(listaDeElementos)
+// }
+
+// retornaTodosElementos()
+
+///////////////EXERCICIO 3////////////
+
+// function retornaValorEpecifico(num){
+//     let value = listaDeElementos[num]
+//     if (value){
+//         return `O valor contido nesta posição é: ${value}".`
+//     } else {
+//         return `Valor invalido`
+//     }
+// }
+
+// let numero = parseInt(prompt(`Digite um numero inteiro`))
+
+// console.log(retornaValorEpecifico(numero))
+
+///////////////EXERCICIO 4////////////
