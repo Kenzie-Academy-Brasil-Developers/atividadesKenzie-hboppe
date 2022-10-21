964441dbf0f62c948acbca24a30ac2f8df0bef2c
hboppe <hanna.nascimento31@gmail.com>
2022-10-20 20:48:13 -0400

    fiz ate exercicio 3

---
+++ s4-22/script.js
@@ -0,0 +1,105 @@
+// ///////////////EXERCICIO 1 //////////////
+
+// const Pessoa = {
+//     nome: 'Hanna',
+//     anoDeNascimento: 1995,
+//     cpf: '023.266.559-55',
+//     cidade: 'Winchester',
+//     estado: 'Virginia',
+//     logradouro: 'Valley Road',
+// }
+
+// ///////////////EXERCICIO 2 //////////////
+
+// const Escola = {
+//     nome: 'John Wood Highschool',
+//     cnpj: '33.200.056/0001-49',
+//     areaDeAtuacao: 'Ensino medio',
+//     cidade: 'Winchester',
+//     estado: 'Virginia',
+//     logradouro: 'Pleasant Valley',
+//     curso: 'Matematica',
+// }
+
+// ///////////////EXERCICIO 3 //////////////
+
+
+// const Curso = {
+//     nome: 'Matematica',
+//     duracaoEmAnos: 4,
+//     turma: 1,
+//     modulos: 5,
+
+// }
+
+// ///////////////EXERCICIO 4 //////////////
+
+
+// const Endereco = {
+//     cidade: 'Winchester',
+//     estado: 'Virginia',
+//     logradouro: 'Rua da compadecida',
+//     cep: '60.822-770',
+// }
+
+// ///////////////EXERCICIO 5 //////////////
+
+
+// const Turma = {
+//     professores:['Thiago', 'Hudson', 'Marcos'],
+//     alunos:['Julia', 'Kevin', 'Gabs'],
+//     dataDeInicio: '15/09/2022',
+//     dataPrevistaDeFormatura: '15/09/2023',
+// }
+
+// ---------------MANIPULACAO --------------------
+
+
+const figure = {  
+    name: "Carl",   
+    classes: ["Warrior"],  
+    leader_trend: true,
+    power: 6, 
+    allied: true  
+}
+
+///////////EXERCICIO 1/////////
+
+// function returnName(){
+//     return figure.name;
+// }
+
+// console.log(returnName())
+
+///////////EXERCICIO 2/////////
+
+// function verifyClasses(){
+//     if (figure.classes.length > 1){
+//         return figure.classes
+//     } else {
+//         return figure.classes[0];
+//     }
+// }
+
+// console.log(verifyClasses())
+
+///////////////////EXERCICIO 3///////////////
+
+// function realPower(){
+//     if(figure.leader_trend === true){
+//         let pow = figure.power;
+//         return Math.pow(pow, 2)
+//     } else {
+//         return figure.power;
+//     }
+// }
+
+// console.log(realPower())
+
+///////////////////EXERCICIO 4//////////////////
+
+function inserNewClass(str){
+
+}
+
+
