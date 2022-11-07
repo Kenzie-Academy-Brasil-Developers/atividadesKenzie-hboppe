16c6bfd5e1bc855de2d2aed39d3c09c3edf346a7
hboppe <hanna.nascimento31@gmail.com>
2022-11-07 14:12:26 -0500

    Finalizei exercicio

---
+++ s7-03/script.js
@@ -0,0 +1,8 @@
+let ul = document.createElement('ul');
+let li = document.createElement('li');
+let main = document.getElementById('root')
+
+li.innerText = "repetir cada vez mais";
+
+ul.appendChild(li)
+main.appendChild(ul)
