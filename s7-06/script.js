f86eaeb87a5dd584ef7d8160d83118dfbdfd7dc8
hboppe <hanna.nascimento31@gmail.com>
2022-11-08 15:26:45 -0500

    Finalizei exercicio

--- s7-06/script.js
+++ s7-06/script.js
@@ -1,7 +1,7 @@
 let tasks = [
     {
       titulo: "Comprar comida para o gato",
-      tipo: "Urgente"
+      tipo: "urgente"
     },
     {
       titulo: "Limpar o quarto",
@@ -13,10 +13,31 @@ let tasks = [
     },  
     {
       titulo: "Guardar dinheiro do lanche",
-      tipo: "Urgente"
+      tipo: "urgente"
     },  
     {
       titulo: "Beber água",
       tipo: "prioritário"
     }    
-  ]
+]
+
+let ul = document.createElement('ul');
+let section = document.getElementById('tarefas');
+
+
+let urgent = tasks.filter(tk => tk.tipo == 'urgente');
+let prio = tasks.filter((tk) => tk.tipo == 'prioritário');
+let nor = tasks.filter((tk) => tk.tipo == 'normal');
+let array = [...urgent, ...prio, ...nor]
+console.log(array)
+
+
+section.appendChild(ul);
+
+
+for(let i = 0; i < array.length; i++){
+  let li = document.createElement('li');
+  ul.appendChild(li)
+  li.innerHTML = `${array[i].titulo}`
+  li.classList.add(array[i].tipo)
+}
